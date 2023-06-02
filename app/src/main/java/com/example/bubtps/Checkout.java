be6e005a433735c99dfc6043aa7e1e4f1c9ca94f
IFTEKHAR AHAMED SIDDIQUEE <70750068+Iftekhar-Ahamed@users.noreply.github.com>
2023-06-02 20:41:49 +0600

    qr code genarate

--- app/src/main/java/com/example/bubtps/Checkout.java
+++ app/src/main/java/com/example/bubtps/Checkout.java
@@ -3,12 +3,29 @@ package com.example.bubtps;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+import android.widget.ImageView;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import androidmads.library.qrgenearator.QRGContents;
+import androidmads.library.qrgenearator.QRGEncoder;
 public class Checkout extends AppCompatActivity {
-
+    private ImageView pic ;
+    private Bitmap bitmap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checkout);
+
+
+
+        String s = "19202103239";
+        pic = findViewById(R.id.imageViewcheckout);
+        QRGEncoder qrgEncoder = new QRGEncoder(s, null, QRGContents.Type.TEXT, 1000);
+        pic.setImageBitmap(qrgEncoder.getBitmap());
+
+        qrgEncoder.setColorBlack(Color.WHITE);
+        qrgEncoder.setColorWhite(Color.BLACK);
+        bitmap = qrgEncoder.getBitmap();
+        pic.setImageBitmap(bitmap);
     }
 }
--- app/src/main/java/com/example/bubtps/checkin.java
+++ app/src/main/java/com/example/bubtps/checkin.java
@@ -3,12 +3,32 @@ package com.example.bubtps;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import androidmads.library.qrgenearator.QRGContents;
+import androidmads.library.qrgenearator.QRGEncoder;
 
 public class checkin extends AppCompatActivity {
-
+    private ImageView pic;
+    private Bitmap bitmap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checkin);
+
+
+
+
+        String s = "19202103239";
+        pic = findViewById(R.id.imageViewCheckin);
+        QRGEncoder qrgEncoder = new QRGEncoder(s, null, QRGContents.Type.TEXT, 1500);
+        //pic.setImageBitmap(qrgEncoder.getBitmap());
+
+        qrgEncoder.setColorBlack(Color.WHITE);
+        qrgEncoder.setColorWhite(Color.BLACK);
+        bitmap = qrgEncoder.getBitmap();
+        pic.setImageBitmap(bitmap);
+
     }
 }
