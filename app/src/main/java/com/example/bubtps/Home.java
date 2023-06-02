b07d037f763ef9b1ce9a08f69b1d0e9525083a1c
IFTEKHAR AHAMED SIDDIQUEE <70750068+Iftekhar-Ahamed@users.noreply.github.com>
2023-06-02 16:37:36 +0600

    2-6-23

--- app/src/main/java/com/example/bubtps/Home.java
+++ app/src/main/java/com/example/bubtps/Home.java
@@ -2,13 +2,51 @@ package com.example.bubtps;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Home extends AppCompatActivity {
-
+    private Button profile,checkin,checkout,status;
+    Intent home;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        profile = findViewById(R.id.btn_profile);
+        checkin = findViewById(R.id.btn_checkin);
+        checkout = findViewById(R.id.btn_checkout);
+        status = findViewById(R.id.btn_status);
+
+        profile.setOnClickListener(new View.OnClickListener(){
+            public  void onClick(View v){
+                home = new Intent(Home.this, Profile.class);
+                startActivity(home);
+            }
+        });
+
+        checkin.setOnClickListener(new View.OnClickListener(){
+            public  void onClick(View v){
+                home = new Intent(Home.this, checkin.class);
+                startActivity(home);
+            }
+        });
+
+        checkout.setOnClickListener(new View.OnClickListener(){
+            public  void onClick(View v){
+                Intent home = new Intent(Home.this, Checkout.class);
+                startActivity(home);
+            }
+        });
+        status.setOnClickListener(new View.OnClickListener(){
+            public  void onClick(View v){
+                Intent home = new Intent(Home.this, Status.class);
+                startActivity(home);
+            }
+        });
+
+
     }
 }
