005904ea1161b033c4d9ac50e6a2454665112062
Iftekhar-Ahamed <iftekharahamed56@gmail.com>
2023-06-17 19:29:16 +0600

    checkin -> popup -> slot ->qrcode

--- app/src/main/java/com/example/bubtps/Home.java
+++ app/src/main/java/com/example/bubtps/Home.java
@@ -1,14 +1,19 @@
 package com.example.bubtps;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.TextureView;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class Home extends AppCompatActivity {
-    private Button profile,checkin,checkout,status;
+    private Button profile,checkout,status;
+    Dialog checkindialog;
     Intent home;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,9 +21,9 @@ public class Home extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
         profile = findViewById(R.id.btn_profile);
-        checkin = findViewById(R.id.btn_checkin);
         checkout = findViewById(R.id.btn_checkout);
         status = findViewById(R.id.btn_status);
+        checkindialog = new Dialog(this);
 
         profile.setOnClickListener(new View.OnClickListener(){
             public  void onClick(View v){
@@ -27,12 +32,6 @@ public class Home extends AppCompatActivity {
             }
         });
 
-        checkin.setOnClickListener(new View.OnClickListener(){
-            public  void onClick(View v){
-                home = new Intent(Home.this, checkin.class);
-                startActivity(home);
-            }
-        });
 
         checkout.setOnClickListener(new View.OnClickListener(){
             public  void onClick(View v){
@@ -46,7 +45,32 @@ public class Home extends AppCompatActivity {
                 startActivity(home);
             }
         });
+    }
+    public void showDialog(View v){
+        TextView close;
+        AppCompatButton bike,car;
+        checkindialog.setContentView(R.layout.activity_bike_car);
+        close=(TextView) checkindialog.findViewById(R.id.button_cancel);
+        bike=checkindialog.findViewById(R.id.button_bike);
+        car=checkindialog.findViewById(R.id.button_car);
 
-
+        close.setOnClickListener(new View.OnClickListener(){
+            public  void onClick(View vi){
+                checkindialog.dismiss();
+            }
+        });
+        bike.setOnClickListener(new View.OnClickListener(){
+            public  void onClick(View vi){
+                home = new Intent(Home.this, available_slots.class);
+                startActivity(home);
+            }
+        });
+        car.setOnClickListener(new View.OnClickListener(){
+            public  void onClick(View vi){
+                home = new Intent(Home.this, available_slots.class);
+                startActivity(home);
+            }
+        });
+        checkindialog.show();
     }
 }
