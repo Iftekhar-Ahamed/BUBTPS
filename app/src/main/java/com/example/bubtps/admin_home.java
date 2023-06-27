6b2dd338e4c561f268941e1abba07db8dd66ab3d
Iftekhar-Ahamed <iftekharahamed56@gmail.com>
2023-06-27 13:41:19 +0600

    Admin -> status -> qr scan

--- app/src/main/java/com/example/bubtps/admin_home.java
+++ app/src/main/java/com/example/bubtps/admin_home.java
@@ -14,11 +14,17 @@ public class admin_home extends AppCompatActivity {
         Bundle data = getIntent().getExtras();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_home);
-        Button status = findViewById(R.id.button_adminstatus),scan=findViewById(R.id.button_scan);
+        Button status = findViewById(R.id.button_adminstatus),scan=findViewById(R.id.button_scan),reg=findViewById(R.id.button_register);
 
         scan.setOnClickListener(new View.OnClickListener(){
             public  void onClick(View v){
                 Intent status = new Intent(admin_home.this, scanQrcode.class);
+                startActivity(status);
+            }
+        });
+        reg.setOnClickListener(new View.OnClickListener(){
+            public  void onClick(View v){
+                Intent status = new Intent(admin_home.this, Registration.class);
                 startActivity(status);
             }
         });
